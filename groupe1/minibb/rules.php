50a0b1f075465fb130db7cea4895db86e0916c47
dre@m_m@n <dre@m_m@n>
2016-04-06 14:16:34 +0200

    maj

---
+++ groupe1/minibb/rules.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head><title>Règlement du forum</title>
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/png" href="favicon.png" />
+	<!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="favicon.ico" /><![endif]-->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/navbar-static-top.css" rel="stylesheet">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body >
+	<nav class="navbar navbar-default navbar-static-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="index.php">Ensemble pour l'Afrique</a>
+			</div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="index.php"><span class="glyphicon glyphicon-arrow-left"></span> Revenir sur le forum</a></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div><!--/.navbar-collapse -->
+		</div>
+	</nav>
+	<div class="container">
+		<!-- Example row of columns -->
+		<div class="row">
+			<table class="table table-hover">
+				<tr>
+					<td>
+						<h1>Règlement du forum</h1>
+
+						<h2>Article 0. Préface</h2>
+
+						La règlementation ci-dessous a été établie dans le but principal de réunir l’ensemble de la communauté d'Ensemble Pour l'Afrique au sein d’un forum placé sous le signe du respect d’autrui et de la convivialité, afin de favoriser l’échange et le partage de connaissances. En s’inscrivant sur cette plateforme, l’utilisateur déclare avoir pris connaissance des règles qui le régissent ainsi que de les avoir comprises et assimilées de manière à pouvoir les appliquer à tout temps et à tout moment.
+
+						<br /><br />
+
+						Tout utilisateur allant à l’encontre des règles générales, permettant le bon maintien du forum, encoure des sanctions pouvant aller du simple avertissement au bannissement permanent du compte forum. Ceci s’applique également aux règles spécifiques de chaque section. L’équipe de modération se réserve le droit de modifier et déplacer tout message en infraction aux règles, ainsi que de bloquer l’accès de comptes forums dans les cas les plus graves.
+
+
+						<br /><br />
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+
+						<h2>Article 1. Comptes</h2>
+
+						1.1 De par le système actuellement en place, chaque utilisateur a le droit d’enregistrer et utiliser un seul et unique compte par l’intermédiaire de son compte jeu. Générer des comptes multiples sera punissable et reviendra à du multi-compte sur le plan jeu. Si le joueur possède plusieurs comptes jeu inscrits sur des mondes différents, il ne pourra en enregistrer et n'en utiliser qu'un seul sur le forum.
+
+						<br /><br />
+
+						1.2 Chaque utilisateur a des données personnelles associées à son compte (identifiants, e-mail, etc.). Ce dernier n’est pas autorisé à divulguer ces informations le concernant ni même celles concernant un autre utilisateur.
+
+
+						<br /><br />
+					</td>
+				</tr>
+				<tr>
+					<td>
+
+						<h2>Article 2. Attitude</h2>
+						2.1 La langue officielle de la communauté est le français, seule cette langue peut être utilisée pour la communication au sein du forum. Une citation dans une langue étrangère peut être acceptée si sa traduction en français est affichée au même endroit (à côté, en-dessous ...). Ceci est également valable pour les signatures et autres éléments personnalisables du profil.
+						<br /><br />
+						2.2 Le langage dit « sms » ou « msn » est à proscrire. Pour certains c’est une manière plus rapide de communiquer mais pour d’autres c’est très difficile à déchiffrer. Ne saccageons pas notre bel héritage qu’est la langue de Molière.
+						<br /><br />
+						2.3 L’utilisateur du forum se doit de respecter autrui et de rester poli en toute circonstance. Les insultes personnelles (même partiellement masquées mais parfaitement compréhensibles), l’agressivité et la provocation envers un utilisateur quelconque du forum ou un membre de l’équipe de modération seront sanctionnées.
+						<br /><br />
+						2.4 Se faire passer pour un modérateur ou tout autre membre de l’équipe est interdit. L’usurpation d’identité que ce soit par pseudonyme, avatar, signature ou comportement n’est pas tolérée et sera sanctionnée.
+						<br /><br />
+						2.5 En plus des présentes règles, chaque section du forum dispose de ses propres règles spécifiques. L'utilisateur est prié de les lire et se doit de les respecter au même titre que les règles générales du forum.
+						<br /><br />
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+
+						<h2>Article 3. Messages</h2>
+
+						3.0 Le hors-sujet, le flood et le spam sont ainsi définis :
+						- le hors-sujet est un message ou un ensemble de messages qui s'écartent de la ligne directrice définie par le titre et le premier message de la discussion ; bien qu'il soit constructif ou apporte des éléments intéressants, mais qui ne sont pas en rapport avec la discussion en cours ;
+						- le flood est un message ou un ensemble de messages n'apportant rien de constructif ou d'intéressant à la discussion en cours et généralement hors-sujet, sans pour autant être malveillant ;
+						- le spam est l'envoi répété de messages dans le but de nuire ou de perturber la bonne lisibilité du forum d'une manière générale ou de la messagerie privée d'un utilisateur.
+
+						<br /><br />
+
+						3.1 Tout message (et plus généralement tout contenu publié sur le forum par utilisateur) se verra modifié, censuré, déplacé, supprimé dans le cas où :
+						- Le message est contraire à l’ordre public et aux bonnes mœurs ;
+						- Le message est à caractères pornographique, xénophobe, extrémiste politique ou religieux, révisionniste, immoral, injurieux, offensant, diffamatoire, raciste ou portant atteinte à l’honneur d’autrui ;
+						- Le message incite à la discrimination d’une personne ou d’un groupe de personne pour des raisons d’origine, de race, de religion, de mode de vie ou d’appartenance ou non à une ethnie ;
+						- Le message contient des informations au sujet de clefs, hacks, cracks ou toute autre chose de nature illégale ;
+						- Le message rentre dans le cadre d’un règlement de compte ou un conflit entre utilisateurs.
+
+
+
+						<br /><br />
+
+						3.2 Publier des contenus n’appartenant pas au sujet ou à la section concernée (flood, hors-sujet, etc.) est à éviter. N’est pas concerné par cette règle la section Détente.
+
+						<br /><br />
+
+						3.3 Spammer le forum de messages sans intérêt dans le simple but de mettre du désordre est strictement interdit sur l'ensemble du forum.
+
+						<br /><br />
+
+						3.4 L’utilisateur doit faire attention à ne pas publier d’image dépassant le cadre normal (nécessitant de naviguer dans la fenêtre par l’intermédiaire des barres de défilement horizontales et verticales). S'il ne lui est pas possible de redimensionner l'image, ou bien si le redimensionnement altérait la lisibilité de l'image, il lui est demandé de poster à la place un lien externe vers cette image.
+
+						<br /><br />
+
+						3.5 Il est interdit aux utilisateurs de créer un nouveau sujet pour une discussion déjà fermée. Si vous souhaitez apporter un complément à la discussion qui nécessite la réouverture de cette dernière, faites en part au modérateur ayant décidé la fermeture de la discussion, qui prendra la décision appropriée.
+
+						<br /><br />
+
+						3.6 La publicité et les liens pour d'autres sites est interdite. Les liens de parrainage et liens à "clics", qu'ils soient explicites ou non, sont également interdits.
+
+						<br /><br />
+
+						3.7 La publicité pour des blogs, forums communautaires et autres est permise, mais uniquement dans la section Détente, et à condition qu'il ne s'agisse pas d'un contenu lié à un quelconque jeu (Exemple de site permis : forum de jeu de rôle). Les vidéos Youtube (et, par extension, tout autre type de lien vidéo) sont autorisées dans la signature des utilisateurs ainsi que dans un sujet d'échange de liens en section Détente si ces vidéos respectent l'ensemble des présentes règles.
+
+						<br /><br />
+
+						3.8 En cas de soucis de manipulation sur le forum (double postage, discussions doublons, etc.), contactez un modérateur de la section. Si un utilisateur poste deux messages à la suite dans un délai réduit, ceux-ci seront automatiquement fusionnés. Cela ne doit pas inciter l'utilisateur à ne pas faire attention à ce qu'il fait. Un excès de double-posts ou de discussions doublons malgré un rappel à l'ordre sera sanctionné.
+
+						<br /><br />
+
+						3.9 Poster un message au nom d’un utilisateur banni n’est pas autorisé.
+
+						<br /><br />
+
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+
+						<h2>Article 4. Messagerie privée </h2>
+
+						4.1 La publicité et le harcèlement par l’intermédiaire de la messagerie privée pourront être sanctionnés.
+
+						<br /><br />
+
+						4.2 La copie partielle ou totale d’un message privé ne peut être rendue publique sans l’accord des deux partis.
+
+
+						<br /><br />
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+
+						<h2>Article 5. Avatars et signatures</h2>
+
+						5.1 Les images utilisées en guise d’avatar et de signature sont soumises aux mêmes règles que les messages. Dans le cas où un avatar et/ou une signature seraient contraire aux règles, il(elle) devra être changée sur demande de l’équipe de modération.
+						<br /><br />
+						5.2 Une seule image est autorisée pour la signature.
+						<br /><br />
+						5.3 Les images utilisées devront être fixes (non-animées).
+						<br /><br />
+						5.4 L’image employée pour l’avatar ne peut excéder les limites suivantes :
+						- Hauteur : 100 pixels ;
+						- Largeur : 100 pixels ;
+						- Poids : 64 kilo octets.
+						<br /><br />
+						5.5 L’image employée pour la signature ne peut excéder les limites suivantes :
+						- Hauteur : 120 pixels ;
+						- Largeur : 700 pixels ;
+						- Poids : 150 kilo octets.
+						<br /><br />
+						5.6 En plus d'une image, la signature peut contenir du texte ou des sauts de ligne dans une proportion raisonnable (quatre lignes maximum). L'utilisation de la balise [SIZE] n'est pas autorisée dans la signature, sauf si la police d'écriture utilisée réduit naturellement la taille et la rend illisible (exemple : Garamond) : dans ce cas uniquement, la balise [SIZE] peut être utilisée pour rétablir une taille lisible, mais pas davantage.
+						<br /><br />
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+
+						<h2>Article 6. Modération du forum</h2>
+						6.1.1 Les membres de l’équipe de modération sont des bénévoles recrutés sur candidature par le responsable de la communauté.
+						<br /><br />
+
+						<br /><br />
+						6.1.2 L’équipe de modération est à traiter avec égalité, les membres qui la composent sont aussi des joueurs comme les autres.
+						<br /><br />
+						6.3 Pour tout problème rencontré sur le forum, l’équipe de modérateurs se tient à disposition de l’utilisateur. Ce dernier ne peut cependant pas les contacter pour des problèmes concernant le jeu, il en retourne de l’équipe des opérateurs dans ces cas-là. De même, l’équipe des opérateurs ne pourra pas aider pour des problèmes relatifs au forum.
+						<br /><br />
+						6.4 En cas de conflit avec l’équipe de modération, l’utilisateur est en mesure de contacter le responsable de la communauté. La création d’une discussion publique à ce sujet n’est pas autorisée.
+						<br /><br />
+						6.5 Lors de l’infraction d’une ou plusieurs règles, l’équipe de modération est en mesure de vous sanctionner en vous attribuant un avertissement.
+						<br /><br />
+						6.6 À l’issue de plusieurs avertissements reçus, un bannissement du compte sera effectué. Le bannissement pourra être prolongé ou devenir permanent dans le cas où l’utilisateur aggraverait son cas.
+						<br /><br />
+						6.7 Si vous pensez avoir reçu un avertissement injustement, prenez contact avec le modérateur qui a donné l'avertissement. Dans le cas où vous n’obtiendriez pas satisfaction auprès du modérateur en question, vous pouvez contacter le responsable de la communauté. Sa décision sera ferme et définitive.
+						<br /><br />
+						6.8 Les bannissements/blocages jeu ou forum n'ont pas à être discutés sur le forum. Prenez contact avec l'opérateur ou la personne qui vous a bloqué via message privé/Ticket de Support/e-mail seulement.
+
+						<br /><br />
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						<h2>Article 7. Divers</h2>
+						7.1 L’équipe de modération et l’ensemble des membres de la communauté ne sont pas des spécialistes dans tous les domaines. N’est fourni sur ce forum qu’un support général.
+						<br /><br />
+						7.2 Les candidatures pour un poste de modérateur forum ou d’opérateur jeu devront se faire via l'outil spécifique lors des périodes de recrutement, qui sont annoncées dans le sujet approprié.
+						<br /><br />
+					</td>
+				</tr>
+
+			</table>
+
+			<footer>
+				<p>&copy; 2016 Paris - Dauphine</p>
+			</footer>
+		</div><!-- /.row -->
+
+	</div><!-- /.container -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+	<script src="js/bootstrap.min.js"></script>
+	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	<script src="js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
