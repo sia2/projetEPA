af867848acbf250bbfda3185eb7bceaaa792e96d
MedSyl10 <mohamed.sylla.info@gmail.com>
2016-04-28 03:24:21 +0200

    version sans Diarra

--- groupe4/connected.php
+++ groupe4/connected.php
@@ -28,8 +28,8 @@
               }
               if($_SESSION['status'] != 'President' && $_SESSION['status'] != 'Tresorier' && $_SESSION['status'] != 'Secretaire' && $_SESSION['status'] != 'Membre CA') {
         ?>
-                <a class="btn btn-primary" href="../groupe3/FormFr.html">Vous êtes étudiant et souhaitez faire une demande d'accueil</a><br><br>
-                <a class="btn btn-primary" href="../groupe5/liste-projet.php">Liste des projets</a><br><br>
+                <a class="btn btn-p/FormFr.html">Vous êtes étudiant et souhaitez faire une demande d'accueil</a><br><br>
+                <a class="btn btn-primary" href="../groupe5/liste-projet.php">Gestion des projets</a><br><br>
         <?php
               }
                 if(isset($_SESSION['status']) && ($_SESSION['status'] == 'President' || $_SESSION['status'] == 'Secretaire' || $_SESSION['status'] == 'Tresorier' || $_SESSION['status'] == 'Membre CA')) {
@@ -37,6 +37,7 @@
                   <a class="btn btn-info" href="membershipDemand.php">Gestion des demandes d'adhésions</a><br><br>
                   <a class="btn btn-success" href="membershipList.php">Liste des adhérents</a><br><br>
                   <a class="btn btn-success" href="membershipGestion.php">Gestion des adhérents</a><br><br>
+                  <a class="btn btn-success" href="../groupe5/liste-projet.php">Gestion des projets</a><br><br>
         <?php
                 }
                 if($_SESSION['status'] == 'Tresorier') {
